eb2a6586dd274de2961505ab9650ecf7427fde4a
mihaistancu <mihai-dorian.stancu@ext.ec.europa.eu>
2024-09-08 21:26:10 +0200

    add back headers

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,6 +8,7 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     public String UserId { get; set; }
+    public String Url { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -16,6 +17,10 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        Url = "Headers ";
+        foreach (var header in Request.Headers)
+            Url += header.Key + "=" + header.Value + Environment.NewLine;
+
         UserId = User.Identity.IsAuthenticated 
             ? User.FindFirst(ClaimTypes.NameIdentifier).Value
             : "Guest";
