b369ed2f70a6ce050e3b00c74a084d54a76662f4
mihaistancu <mihai-dorian.stancu@ext.ec.europa.eu>
2024-09-08 17:58:26 +0200

    Another attempt at identity

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace psyrdv.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public String UserId { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -14,11 +16,8 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-        var userName = User.Identity.IsAuthenticated 
-            ? "Mihai Stancu"
+        UserId = User.Identity.IsAuthenticated 
+            ? User.FindFirst(ClaimTypes.NameIdentifier).Value
             : "Guest";
-        
-        //ViewBag.UserName = userName;
-        ViewData["UserName"] = userName;
     }
 }
