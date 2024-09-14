4287deac21361221fe452f715ed6958624f1ccc3
mihaistancu <mihai-dorian.stancu@ext.ec.europa.eu>
2024-09-14 13:13:31 +0200

    Try to write to db

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
+using Microsoft.Data.SqlClient;
 
 namespace psyrdv.Pages;
 
@@ -18,5 +19,24 @@ public class IndexModel : PageModel
     public void OnGet()
     {
         UserId = Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"];
+        
+        try {
+            using var conn = new SqlConnection("Server=tcp:psyrdv.database.windows.net,1433;Initial Catalog=bookings;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";");
+            conn.Open();
+
+            var command = new SqlCommand(
+                "INSERT INTO access (accessed, userid) VALUES (@accessed, @userid)",
+                conn);
+
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@accessed", DateTime.Now);
+            command.Parameters.AddWithValue("@userid", UserId);
+
+            using SqlDataReader reader = command.ExecuteReader();
+        }
+        catch (Exception ex) {
+            UserId += ex.Message;            
+        }
+        
     }
 }
