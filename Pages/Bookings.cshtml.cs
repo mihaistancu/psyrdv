80ee9adc047f8ba4cd843a488b4ff3273883903d
mihaistancu <mihai-dorian.stancu@ext.ec.europa.eu>
2024-10-06 19:13:51 +0200

    Show patient names in rdv list

--- Pages/Bookings.cshtml.cs
+++ Pages/Bookings.cshtml.cs
@@ -7,22 +7,34 @@ public class BookingsModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IBookingsRepo _bookingsRepo;
+    private readonly IPatientsRepo _patientsRepo;
 
     public List<Booking> Bookings { get; set; } = new List<Booking>();
-    
-    public BookingsModel(ILogger<IndexModel> logger, IBookingsRepo bookingsRepo)
+
+    public Dictionary<Guid, Patient> Patients { get; set; } = new Dictionary<Guid, Patient>();
+
+    public BookingsModel(
+        ILogger<IndexModel> logger, 
+        IBookingsRepo bookingsRepo, 
+        IPatientsRepo patientsRepo)
     {
         _logger = logger;
         _bookingsRepo = bookingsRepo;
+        _patientsRepo = patientsRepo;
     }
 
     public void OnGet()
     {        
         try {
            Bookings = _bookingsRepo.GetAll();
+           Patients = _patientsRepo.GetAll().ToDictionary(p => p.Id, p => p);
         }
         catch (Exception ex) {
             _logger.LogError(ex.Message);
         }
+    }
+
+    public String PatientName(Booking booking) {
+        return Patients[booking.PatientId].FirstName + " " + Patients[booking.PatientId].LastName;
     }
 }
