85261e8c9aeb3b39fdeb7072111898b882d27cd1
mihaistancu <mihai-dorian.stancu@ext.ec.europa.eu>
2024-10-06 17:32:03 +0200

    Rename create to add

--- Pages/AddBooking.cshtml.cs
+++ Pages/AddBooking.cshtml.cs
@@ -4,7 +4,7 @@ using psyrdv.Data;
 
 namespace psyrdv.Pages;
 
-public class CreateBookingModel : PageModel
+public class AddBookingModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IBookingsRepo _bookingsRepo;
@@ -12,7 +12,7 @@ public class CreateBookingModel : PageModel
     [BindProperty]
     public Booking Booking { get; set; }
     
-    public CreateBookingModel(ILogger<IndexModel> logger, IBookingsRepo bookingsRepo)
+    public AddBookingModel(ILogger<IndexModel> logger, IBookingsRepo bookingsRepo)
     {
         _logger = logger;
         _bookingsRepo = bookingsRepo;
